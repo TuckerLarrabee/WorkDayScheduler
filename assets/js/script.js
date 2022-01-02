837445a63377a9fa3712001063b1ea9b023cad88
TCL git <tuckerlarrabee13@gmail.com>
2022-01-02 13:56:44 -0500

    background color and storage start

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,63 @@
-$(".text").on("click", function() {
-    var text = $(this).text().trim();
+var tasks = {};
 
-    var textInput = $("<textArea>").addClass("col-8").addClass("format").val(text);
+var textBlock = $(".description");
 
-    $(this).replaceWith(textInput);
+var currentHour = moment().hour()
 
-    textInput.trigger("focus");
+var time = moment().format("[Today's date is] dddd, MMMM Do")
+
+var timeDisplay = $("#currentDay");
+timeDisplay.text(time);
+
+var dateCheck = function () {
+    //get date from each row
+    var timeBlock = $(".hour");
+    // console.log(timeBlock)
+
+    for(var i = 0; i < timeBlock.length; i++) {
+        var hour = parseInt($(timeBlock[i]).attr("id"));
+       
+        if (hour < currentHour) {
+            $(textBlock[i]).addClass("past");
+        } else if (hour == currentHour) {
+            $(textBlock[i]).addClass("present");
+        } else {
+            $(textBlock[i]).addClass("future");
+        }
+    }
+}
+
+var saveTasks = function () {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  };
+
+$(".saveBtn").on("click", function () {
+    //if we click the save button, grab the text from the same row
+    var taskSave = ($(this).siblings()[1].textContent.trim());
+    console.log(taskSave);
+
+    //after we grab the text from the same row, push that into an array
+    tasks.push(taskSave)
+    console.log(tasks)
+
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+
+
+    // for (var i = 0; i < textBlock.length; i++) {
+    //     var test = $(textBlock[i]).val().trim();
+    //     console.log(test)
+    // }
+
+    // localStorage.setItem("tasks", JSON.stringify(tasks))
+
+
+
+    // for(var i = 0; i < textBlock.length; i++) {
+    //     var data = ($(textBlock[i]).val());
+    //     console.log(data);
+    // }
 })
+
+
+
+dateCheck();  
